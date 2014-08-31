d0507e7d846c7c666a124c94d1a3dd8cdf52d2fc
Tony Shen <fengzhizi715@126.com>
2014-08-31 03:00:37 -0500

    增加pom文件

---
+++ src/cn/salesuite/saf/log/L.java
@@ -0,0 +1,183 @@
+/**
+ * 
+ */
+package cn.salesuite.saf.log;
+
+import cn.salesuite.saf.app.SAFActivity;
+import cn.salesuite.saf.app.SAFFragment;
+import cn.salesuite.saf.app.SAFFragmentActivity;
+import cn.salesuite.saf.utils.SAFUtil;
+import cn.salesuite.saf.utils.StringHelper;
+import android.util.Log;
+
+/**
+ * @author Tony Shen
+ *
+ */
+public class L {
+	
+	public static enum LogLevel {
+		ERROR {
+			public int getValue() {
+				return 0;
+			}
+		},
+		WARN {
+			public int getValue() {
+				return 1;
+			}
+		},
+		INFO {
+			public int getValue() {
+				return 2;
+			}
+		},
+		DEBUG {
+			public int getValue() {
+				return 3;
+			}
+		};
+
+		public abstract int getValue();
+	};
+	
+	private static String TAG = "SAF_L";
+	
+	public static LogLevel logLevel = LogLevel.DEBUG; // 日志的等级，可以进行配置
+	
+	public static void init(SAFActivity activity) {
+		TAG = activity.TAG;
+	}
+	
+	public static void init(SAFFragmentActivity activity) {
+		TAG = activity.TAG;
+	}
+	
+	public static void init(SAFFragment fragment) {
+		TAG = fragment.TAG;
+	}
+	
+	public static void e(String msg) {
+		if (LogLevel.ERROR.getValue() <= logLevel.getValue()) {
+			
+			if(StringHelper.isNotBlank(msg)) {
+				Log.e(TAG, msg);
+			}
+		}
+	}
+	
+	public static void e(String msg,Throwable tr) {
+		if (LogLevel.ERROR.getValue() <= logLevel.getValue()) {
+			
+			if(StringHelper.isNotBlank(msg)) {
+				Log.e(TAG, msg,tr);
+			}
+		}
+	}
+	
+	/**
+	 * 打印普通java对象
+	 * @param object
+	 */
+	public static void e(Object object) {
+		if (LogLevel.ERROR.getValue() <= logLevel.getValue()) {
+			
+			if(object!=null) {
+				Log.e(TAG, SAFUtil.printObject(object));
+			}
+		}
+	}
+	
+	public static void w(String msg) {
+		if (LogLevel.WARN.getValue() <= logLevel.getValue()) {
+			
+			if(StringHelper.isNotBlank(msg)) {
+				Log.w(TAG, msg);
+			}
+		}
+	}
+	
+	public static void w(String msg,Throwable tr) {
+		if (LogLevel.WARN.getValue() <= logLevel.getValue()) {
+			
+			if(StringHelper.isNotBlank(msg)) {
+				Log.w(TAG, msg,tr);
+			}
+		}
+	}
+	
+	/**
+	 * 打印普通java对象
+	 * @param object
+	 */
+	public static void w(Object object) {
+		if (LogLevel.WARN.getValue() <= logLevel.getValue()) {
+			
+			if(object!=null) {
+				Log.w(TAG, SAFUtil.printObject(object));
+			}
+		}
+	}
+	
+	public static void i(String msg) {
+		if (LogLevel.INFO.getValue() <= logLevel.getValue()) {
+			
+			if(StringHelper.isNotBlank(msg)) {
+				Log.i(TAG, msg);
+			}
+		}
+	}
+	
+	public static void i(String msg,Throwable tr) {
+		if (LogLevel.INFO.getValue() <= logLevel.getValue()) {
+			
+			if(StringHelper.isNotBlank(msg)) {
+				Log.i(TAG, msg,tr);
+			}
+		}
+	}
+	
+	/**
+	 * 打印普通java对象
+	 * @param object
+	 */
+	public static void i(Object object) {
+		if (LogLevel.INFO.getValue() <= logLevel.getValue()) {
+			
+			if(object!=null) {
+				Log.i(TAG, SAFUtil.printObject(object));
+			}
+		}
+	}
+	
+	public static void d(String msg) {
+		if (LogLevel.DEBUG.getValue() <= logLevel.getValue()) {
+			
+			if(StringHelper.isNotBlank(msg)) {
+				Log.d(TAG, msg);
+			}
+		}
+	}
+	
+	public static void d(String msg,Throwable tr) {
+		if (LogLevel.DEBUG.getValue() <= logLevel.getValue()) {
+			
+			if(StringHelper.isNotBlank(msg)) {
+				Log.d(TAG, msg,tr);
+			}
+		}
+	}
+	
+	/**
+	 * 打印普通java对象
+	 * @param object
+	 */
+	public static void d(Object object) {
+		if (LogLevel.DEBUG.getValue() <= logLevel.getValue()) {
+			
+			if(object!=null) {
+				Log.d(TAG, SAFUtil.printObject(object));
+			}
+		}
+	}
+}

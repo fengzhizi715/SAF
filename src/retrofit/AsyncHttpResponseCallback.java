09e4e96083d81413aeb4c2444261c986f0251741
Tony Shen <fengzhizi715@126.com>
2014-11-05 01:36:59 -0600

    增加AsyncHttpResponseCallback

---
+++ src/retrofit/AsyncHttpResponseCallback.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package retrofit;
+
+/**
+ * @author Tony Shen
+ * @param <T>
+ *
+ */
+public abstract class AsyncHttpResponseCallback<T> implements Callback<T>{
+
+	public void onStart() {
+	}
+
+	public void onFinish() {
+	}
+}

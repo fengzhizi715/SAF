b3d1762bbd6c365c4a5e4061cdb4189da5976d34
Tony Shen <fengzhizi715@126.com>
2015-03-01 11:57:23 +0800

    Lists增加isBlank()

--- src/cn/salesuite/saf/utils/Lists.java
+++ src/cn/salesuite/saf/utils/Lists.java
@@ -10,9 +10,23 @@ import java.util.List;
  *
  */
 public class Lists {
-
+	
 	/**
 	 * 判断集合是否为空
+	 * @param list
+	 * @return
+	 */
+	public static <T> boolean isBlank(List<T> list) {
+		
+		if (list==null || list.size()==0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * 判断集合是否不为空
 	 * @param list
 	 * @return
 	 */

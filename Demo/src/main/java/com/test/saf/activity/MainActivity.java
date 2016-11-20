3d1f5c8e8284af074afb5db26fd071d1656218f5
Tony Shen <fengzhizi715@126.com>
2016-11-20 10:48:44 +0800

    修改build文件

--- Demo/src/main/java/com/test/saf/activity/MainActivity.java
+++ Demo/src/main/java/com/test/saf/activity/MainActivity.java
@@ -1,11 +1,11 @@
 package com.test.saf.activity;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,35 +13,57 @@ import android.view.View;
 import com.test.saf.R;
 import com.test.saf.app.BaseActivity;
 
+import cn.salesuite.saf.inject.annotation.InjectView;
+
 public class MainActivity extends BaseActivity {
 
-	private DrawerLayout drawerLayout;
-	private View content;
-	private RecyclerView recyclerView;
-	private NavigationView navigationView;
+	@InjectView(id=R.id.drawer_layout)
+	DrawerLayout drawerLayout;
+
+	@InjectView
+	View content;
+
+	@InjectView(id = R.id.navigation_view)
+	NavigationView navigationView;
+
+	@InjectView
+	FloatingActionButton fab;
+
+	@InjectView
+	Toolbar toolbar;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		initViews();
+	}
+
+	private void initViews() {
 		initFab();
 		initToolbar();
-		setupDrawerLayout();
-
-		content = findViewById(R.id.content);
+		navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+			@Override
+			public boolean onNavigationItemSelected(MenuItem menuItem) {
+				Snackbar.make(content, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
+				menuItem.setChecked(true);
+				drawerLayout.closeDrawers();
+				return true;
+			}
+		});
 	}
 
 	private void initFab() {
-		findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
-			@Override public void onClick(View v) {
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
 				Snackbar.make(content, "FAB Clicked", Snackbar.LENGTH_SHORT).show();
 			}
 		});
 	}
 
 	private void initToolbar() {
-		final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 		final ActionBar actionBar = getSupportActionBar();
 
@@ -49,20 +71,6 @@ public class MainActivity extends BaseActivity {
 			actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
 			actionBar.setDisplayHomeAsUpEnabled(true);
 		}
-	}
-
-	private void setupDrawerLayout() {
-		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-		navigationView = (NavigationView) findViewById(R.id.navigation_view);
-		navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-			@Override public boolean onNavigationItemSelected(MenuItem menuItem) {
-				Snackbar.make(content, menuItem.getTitle() + " pressed", Snackbar.LENGTH_LONG).show();
-				menuItem.setChecked(true);
-				drawerLayout.closeDrawers();
-				return true;
-			}
-		});
 	}
 
 }

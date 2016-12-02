828387cf9b366a699514b91fb432c34d8b7757ec
Tony Shen <fengzhizi715@126.com>
2016-12-02 14:04:18 +0800

    优化demo代码

--- Demo/src/main/java/com/test/saf/fragment/ImageLoaderFragment.java
+++ Demo/src/main/java/com/test/saf/fragment/ImageLoaderFragment.java
@@ -1,5 +1,7 @@
 package com.test.saf.fragment;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,6 +12,7 @@ import android.view.ViewGroup;
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 import com.test.saf.R;
+import com.test.saf.adapter.DividerGridItemDecoration;
 import com.test.saf.adapter.ImageLoaderAdapter;
 import com.test.saf.app.BaseFragment;
 import com.test.saf.domain.MMPicsResponse;
@@ -18,6 +21,7 @@ import java.io.IOException;
 
 import cn.salesuite.saf.http.rest.RestClient;
 import cn.salesuite.saf.http.rest.RestUtil;
+import cn.salesuite.saf.http.rest.UrlBuilder;
 import cn.salesuite.saf.inject.Injector;
 import cn.salesuite.saf.inject.annotation.InjectView;
 import cn.salesuite.saf.log.L;
@@ -43,6 +47,8 @@ public class ImageLoaderFragment extends BaseFragment {
 
     int page = 1;
 
+    ProgressDialog progDailog;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_imageloader, container, false);
@@ -55,55 +61,58 @@ public class ImageLoaderFragment extends BaseFragment {
     }
 
     private void initViews() {
+
+        progDailog = ProgressDialog.show(mContext, "Loading", "Please wait...", true);
+        progDailog.setCancelable(false);
+
         fab1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 page++;
-                String url = "http://www.tngou.net/tnfs/api/list"+"?page="+page;
-                new GetPicTask(url)
-                        .success(new RxAsyncTask.SuccessHandler<String>() {
-                            @Override
-                            public void onSuccess(String content) {
-                                try {
-                                    respnose = RestUtil.parseAs(MMPicsResponse.class, content);
-                                    if (respnose != null && Preconditions.isNotBlank(respnose.tngou)) {
-                                        recyclerview.setLayoutManager(new GridLayoutManager(mContext, 2));
-                                        recyclerview.setAdapter(new ImageLoaderAdapter(mContext, respnose.tngou));
-                                        if (faMenu.isOpened()) {
-                                            faMenu.close(true);
-                                        }
-                                    }
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }).failed(new RxAsyncTask.FailedHandler() {
-                    @Override
-                    public void onFail(Throwable e) {
-                        L.e(e.getMessage());
-                    }
-                });
+
+                UrlBuilder urlBuilder = new UrlBuilder("http://www.tngou.net/tnfs/api/list");
+                urlBuilder.parameter("page", page);
+                String url = urlBuilder.buildUrl();
+
+                newGetPicTask(url);
+
+                if (faMenu.isOpened()) {
+                    faMenu.close(true);
+                }
             }
         });
     }
 
     private void initData() {
 
-        new GetPicTask("http://www.tngou.net/tnfs/api/news")
+        String url = "http://www.tngou.net/tnfs/api/news";
+        newGetPicTask(url);
+    }
+
+    private void newGetPicTask(String url) {
+
+        if (progDailog != null && !progDailog.isShowing()) {
+            progDailog = ProgressDialog.show(mContext, "Loading", "Please wait...", true);
+            progDailog.setCancelable(false);
+        }
+
+        new GetPicTask(url,progDailog)
                 .success(new RxAsyncTask.SuccessHandler<String>() {
-            @Override
-            public void onSuccess(String content) {
-                try {
-                    respnose = RestUtil.parseAs(MMPicsResponse.class, content);
-                    if (respnose != null && Preconditions.isNotBlank(respnose.tngou)) {
-                        recyclerview.setLayoutManager(new GridLayoutManager(mContext, 2));
-                        recyclerview.setAdapter(new ImageLoaderAdapter(mContext, respnose.tngou));
+                    @Override
+                    public void onSuccess(String content) {
+                        try {
+                            respnose = RestUtil.parseAs(MMPicsResponse.class, content);
+                            if (respnose != null && Preconditions.isNotBlank(respnose.tngou)) {
+                                recyclerview.setLayoutManager(new GridLayoutManager(mContext,2));
+                                recyclerview.setAdapter(new ImageLoaderAdapter(mContext, respnose.tngou));
+                                recyclerview.addItemDecoration(new DividerGridItemDecoration(mContext));
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).failed(new RxAsyncTask.FailedHandler() {
+                }).failed(new RxAsyncTask.FailedHandler() {
             @Override
             public void onFail(Throwable e) {
                 L.e(e.getMessage());
@@ -115,7 +124,8 @@ public class ImageLoaderFragment extends BaseFragment {
 
         private String apiUrl;
 
-        public GetPicTask(String url) {
+        public GetPicTask(String url,Dialog dialog) {
+            super(dialog);
             this.apiUrl = url;
         }
 

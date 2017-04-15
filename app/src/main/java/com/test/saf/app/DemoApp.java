0dfdb9519211cca67c8179928867e8876b5d6350
fengzhizi715 <fengzhizi715@126.com>
2017-04-15 15:46:35 +0800

    add queue to demo app

--- app/src/main/java/com/test/saf/app/DemoApp.java
+++ app/src/main/java/com/test/saf/app/DemoApp.java
@@ -2,6 +2,7 @@ package com.test.saf.app;
 
 import com.safframework.router.RouterManager;
 import com.safframework.saf.app.SAFApp;
+import com.safframework.saf.queue.Queue;
 
 /**
  * Created by Tony Shen on 15/11/19.
@@ -9,6 +10,8 @@ import com.safframework.saf.app.SAFApp;
 public class DemoApp extends SAFApp {
 
     private static DemoApp mInstance = null;
+
+    public Queue queue;
 
     public static DemoApp getInstance() {
         return mInstance;
@@ -25,5 +28,9 @@ public class DemoApp extends SAFApp {
     private void initData() {
 
         RouterManager.init(this);// 这一步是必须的，用于初始化Router
+
+        // 初始化队列
+        queue = new Queue("saf_queue");
+        queue.start();
     }
 }
